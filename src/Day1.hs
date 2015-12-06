305525c1cd067f41d54619708367bb34c3a2abd5
Taylor Everding <Taylor@Everding.email>
2015-12-05 23:22:55 -0700

    Correct errors in day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -5,14 +5,16 @@ import Data.List (foldl')
 move :: Char -> Integer -> Integer
 move '(' = succ
 move ')' = pred
+move x   = error $ "unknown move type" ++ [x]
 
 floor :: String -> Integer
 floor = foldl' (flip move) 0
 
 basement :: String -> Integer
 basement = fst . foldl' nextMove (-1, 0) . zip [1..]-- foldl' nextMove (-1, 0) pos
-
-nextMove acc@(pos, level) (pos', nextMove)
+  
+nextMove :: (Integer, Integer) -> (Integer, Char) -> (Integer, Integer)
+nextMove acc@(_, level) (pos, nextMove)
   | level == -1 = acc
-  | otherwise = (pos', move nextMove level)
+  | otherwise = (pos, move nextMove level)
 

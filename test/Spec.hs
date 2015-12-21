88cd31d7a9978805fc55e9e119bc9e46e21afa9f
Taylor Everding <Taylor@Everding.email>
2015-12-20 22:44:27 -0700

    Comment old tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -31,17 +31,17 @@ main = defaultMain $ testGroup "Advent of Code" $
   -- , day5
   -- , day6
   -- , day7
-  , day8
+  -- , day8
   -- , day9
   -- , day10
   -- , day11
-  , day12
-  , day13
-  , day14
-  , day15
-  , day16
+  -- , day12
+  -- , day13
+  -- , day14
+  -- , day15
+  -- , day16
   -- , day17
-  , day18
+  -- , day18
   , day20
   ]
 
